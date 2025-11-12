df6587043ad547231231e1e1517a341ff2c043d6
Ranjit Bharat Pathekar <ranjitbharatpathekar@Ranjits-MacBook-Air.local>
2025-11-12 19:54:26 +0530

    Remove old project structure

--- src/store/useTenantStore.ts
+++
@@ -1,4 +0,0 @@
-import create from 'zustand'
-type Tenant = any
-interface TenantState{ tenants:Tenant[]; setTenants:(t:Tenant[])=>void }
-export const useTenantStore = create<TenantState>((set)=>({ tenants:[], setTenants:(t)=>set({ tenants:t }) }))
